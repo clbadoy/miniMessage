691f51dc1935e60a2ecd2915a3a7d491b5fea2d8
clbadoy <clbadoy@cpp.edu>
2022-11-09 21:59:09 -0800

    Beginning to implement a GUI for Admin

--- Admin.java
+++ Admin.java
@@ -1,11 +1,41 @@
-public class Admin {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Admin extends JFrame {
     
     private static Admin instance = null;
+
+    private JPanel upperPanelRegion;
+    private JPanel lowerPanelRegion;
+    private JPanel leftPanelRegion;
+
+    private JButton createUserButton;
+    private JButton createUserGroupButton;
+    private JButton showUserViewButton;
+    private JButton displayTotalUserButton;
+    private JButton displayTotalGroupButton;
+    private JButton positiveMessageRatioButton;
+    private JButton displayTotalMessageCount;
+
+    private JTree treeStructure;
+    private DefaultMutableTreeNode root;
+
+    private ArrayList<User> userList = new ArrayList<User>();
+    private ArrayList<UserGroup> groupList = new ArrayList<UserGroup>();
+
 
     // Constructor that revents classes to create another object of the Admin Class.
     private Admin()
     {
-        
+        initComponents();
     }
 
     public static Admin getInstance() 
@@ -16,5 +46,23 @@ public class Admin {
         }
 
         return instance;
+    }
+
+    private void initComponents() {
+        upperPanelRegion = new JPanel();
+        lowerPanelRegion = new JPanel();
+        leftPanelRegion = new JPanel();
+
+        createUserButton = new JButton();
+        createUserGroupButton = new JButton();
+        showUserViewButton = new JButton();
+        
+        displayTotalUserButton = new JButton();
+        displayTotalGroupButton = new JButton();
+        displayTotalMessageCount = new JButton();
+        positiveMessageRatioButton = new JButton();
+
+        root = new DefaultMutableTreeNode();
+        treeStructure = new JTree(root);
     }
 }
--- UserGroup.java
+++ UserGroup.java
@@ -1,8 +1,33 @@
 import java.util.ArrayList;
 
-public class UserGroup {
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class UserGroup extends DefaultMutableTreeNode {
     
     private String groupID;
-    private ArrayList<User> groupList;
+    private ArrayList<User> userList;
+    private ArrayList<UserGroup> groupRecursiveList;
+
+    public UserGroup(String uid,User creator) {
+        groupID = uid;
+        userList = new ArrayList<User>();
+        groupRecursiveList = new ArrayList<UserGroup>();
+        userList.add(creator);
+    }
+
+    public String getGroupID() {
+        return groupID;
+    }
+    public ArrayList<UserGroup> getGroupList() {
+        return groupRecursiveList;
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void addUser(User user) {
+        userList.add(user);
+    }    
 
 }
