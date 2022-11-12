5a375a417d755fafa735babe7b4271b5d7fc4ce8
clbadoy <clbadoy@cpp.edu>
2022-11-12 10:45:01 -0800

    Test on Main

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Admin.getInstance();
+        User bob = new User("Bob");
+        UserDisplay test = new UserDisplay(bob);
         System.out.println("Hello World");
     }
     
